85a5e961ad51080ddd6d135d47f22c1114f74925
Amine Abbes <amine.abbes94370@gmail.com>
2023-11-24 15:55:09 +0100

    event de scroll, début du responsive tablette

--- index.js
+++ index.js
@@ -1,49 +1,24 @@
 "use strict";
-import {slides, images_bg } from './data.js';
 
-
-
-/*const slides = [
-  {
-    image: "vehicule1.png",
-  },
-  {
-    image: "vehicule2.png",
-  },
-  {
-    image: "vehicule3.png",
-  },
-  {
-    image: "vehicule4.png",
-  },
-];
-
-const images = [
-    "background1.jpg",
-    "background.jpg"
-];*/
+import { slides, images_bg } from './data.js';
 
 const interval = 5000;
-
 let currentIndex = 0;
 const imgElement = document.getElementById("img_banner");
 
 function changeImage() {
-  imgElement.src = "images/"+images_bg[currentIndex];
+  imgElement.src = "images/" + images_bg[currentIndex];
   currentIndex = (currentIndex + 1) % images_bg.length;
 }
 
 setInterval(changeImage, interval);
 
-
 const contain = document.querySelectorAll(".image_arrow_card");
 console.log(contain);
 
-for(let i = 0;i < contain.length;i++){
+for (let i = 0; i < contain.length; i++) {
     createCarousel(contain[i]);
 }
-
-
 
 function createCarousel(container) {
     const interval = 5000;
@@ -54,108 +29,132 @@ function createCarousel(container) {
     console.log(arrowLeft);
     console.log(arrowRight);
 
-
     let img_selected = 0;
 
     arrowRight.addEventListener("click", (e) => {
-        
         if (img_selected == 3) {
             img_selected = 0;
-          } else {
+        } else {
             img_selected++;
-          }
-          imgElement.src = "images/" + slides[img_selected].image;
-          
-          console.log(imgElement.src);
+        }
+        imgElement.src = "images/" + slides[img_selected].image;
+        console.log(imgElement.src);
     });
 
     arrowLeft.addEventListener("click", (e) => {
         if (img_selected == 0) {
             img_selected = 3;
-          } else {
+        } else {
             img_selected--;
-          }
-        
-          imgElement.src = "images/" + slides[img_selected].image;
-          console.log(imgElement.src);
+        }
+
+        imgElement.src = "images/" + slides[img_selected].image;
+        console.log(imgElement.src);
         console.log(imgElement.src);
     });
 }
 
 const handleScroll = (e) => isElementInViewport();
 
+const contourMenu = document.querySelector("#contour_menu");
+
+contourMenu.addEventListener("click", function () {
+    const div_list = document.querySelector("#list_header");
+
+    if (div_list.style.display == "block") {
+        div_list.style.display = "none";
+    } else {
+        div_list.style.display = "block";
+    }
+});
+
 function isElementInViewport() {
     const el = document.querySelector("#img_card_4");
     const rect = el.getBoundingClientRect();
     const container = document.createElement('div');
-    const insertedContent = `<div class="card_voiture">
-    <div class="image_arrow_card">
-        <i class="fa-solid fa-circle-chevron-left arrow_left"></i>
-        <img src="images/vehicule1.png" id="img_card_5" alt="voiture 1">
-        <i class="fa-solid fa-circle-chevron-right arrow_right"></i>
-    </div>
-    <aside>
+    const insertedContent = `<div id="main_deux_partie">
+    <div class="card_voiture">
+        <div class="image_arrow_card">
+            <i class="fa-solid fa-circle-chevron-left arrow_left"></i>
+            <img src="images/vehicule1.png" id="img_card_5" alt="voiture 1">
+            <i class="fa-solid fa-circle-chevron-right arrow_right"></i>
+        </div>
+        <aside>
+        <div class="description">
         <h2 id="titre_card5">Peugeot 208</h2>
         <p>Diesel, 5 portes, GPS, AutoRadio Forfait 1000km (0.5/km supplémentaire)<br><span id="prix_card1">999</span>€ - Agence de Paris</p>
-        <button class="reserver_button">Réserver et payer</button>
-    </aside>
-</div>
-
-<div class="card_voiture">
-    <div class="image_arrow_card">
-        <i class="fa-solid fa-circle-chevron-left arrow_left"></i>
-        <img src="images/vehicule1.png" id="img_card_5" alt="voiture 1">
-        <i class="fa-solid fa-circle-chevron-right arrow_right"></i>
+        </div
+            
+            <button class="reserver_button">Réserver et payer</button>
+        </aside>
     </div>
-    <aside>
-        <h2 id="titre_card5">Peugeot 208</h2>
-        <p>Diesel, 5 portes, GPS, AutoRadio Forfait 1000km (0.5/km supplémentaire)<br><span id="prix_card1">999</span>€ - Agence de Paris</p>
-        <button class="reserver_button">Réserver et payer</button>
-    </aside>
-</div>
 
-<div class="card_voiture">
-    <div class="image_arrow_card">
-        <i class="fa-solid fa-circle-chevron-left arrow_left"></i>
-        <img src="images/vehicule1.png" id="img_card_5" alt="voiture 1">
-        <i class="fa-solid fa-circle-chevron-right arrow_right"></i>
+    <div class="card_voiture">
+        <div class="image_arrow_card">
+            <i class="fa-solid fa-circle-chevron-left arrow_left"></i>
+            <img src="images/vehicule1.png" id="img_card_5" alt="voiture 1">
+            <i class="fa-solid fa-circle-chevron-right arrow_right"></i>
+        </div>
+        <aside>
+        <div class="description">
+        <h2 id="titre_card5">Peugeot 208</h2>
+            <p>Diesel, 5 portes, GPS, AutoRadio Forfait 1000km (0.5/km supplémentaire)<br><span id="prix_card1">999</span>€ - Agence de Paris</p>
+        </div
+            
+            <button class="reserver_button">Réserver et payer</button>
+        </aside>
     </div>
-    <aside>
+
+    <div class="card_voiture">
+        <div class="image_arrow_card">
+            <i class="fa-solid fa-circle-chevron-left arrow_left"></i>
+            <img src="images/vehicule1.png" id="img_card_5" alt="voiture 1">
+            <i class="fa-solid fa-circle-chevron-right arrow_right"></i>
+        </div>
+        <aside>
+        <div class="description">
         <h2 id="titre_card5">Peugeot 208</h2>
-        <p>Diesel, 5 portes, GPS, AutoRadio Forfait 1000km (0.5/km supplémentaire)<br><span id="prix_card1">999</span>€ - Agence de Paris</p>
-        <button class="reserver_button">Réserver et payer</button>
-    </aside>
-</div>
-
-
-<div class="card_voiture">
-    <div class="image_arrow_card">
-        <i class="fa-solid fa-circle-chevron-left arrow_left"></i>
-        <img src="images/vehicule1.png" id="img_card_5" alt="voiture 1">
-        <i class="fa-solid fa-circle-chevron-right arrow_right"></i>
+            <p>Diesel, 5 portes, GPS, AutoRadio Forfait 1000km (0.5/km supplémentaire)<br><span id="prix_card1">999</span>€ - Agence de Paris</p>
+        </div
+            
+            <button class="reserver_button">Réserver et payer</button>
+        </aside>
     </div>
-    <aside>
+
+
+    <div class="card_voiture">
+        <div class="image_arrow_card">
+            <i class="fa-solid fa-circle-chevron-left arrow_left"></i>
+            <img src="images/vehicule1.png" id="img_card_5" alt="voiture 1">
+            <i class="fa-solid fa-circle-chevron-right arrow_right"></i>
+        </div>
+        <aside>
+        <div class="description">
         <h2 id="titre_card5">Peugeot 208</h2>
-        <p>Diesel, 5 portes, GPS, AutoRadio Forfait 1000km (0.5/km supplémentaire)<br><span id="prix_card1">999</span>€ - Agence de Paris</p>
-        <button class="reserver_button">Réserver et payer</button>
-    </aside>
+            <p>Diesel, 5 portes, GPS, AutoRadio Forfait 1000km (0.5/km supplémentaire)<br><span id="prix_card1">999</span>€ - Agence de Paris</p>
+        </div
+            
+            <button class="reserver_button">Réserver et payer</button>
+        </aside>
+    </div>
 </div>`
-container.innerHTML = insertedContent;
+    container.innerHTML = insertedContent;
     console.log(rect.top <= window.screen.height);
-    if(rect.top <= window.screen.height){
+    if (rect.top <= window.screen.height) {
         console.log('test')
         window.removeEventListener("scroll", handleScroll);
         console.log('test after remove')
-        document.querySelector('main').insertAdjacentElement('beforeend',container);
-        
+        document.querySelector('main').insertAdjacentElement('beforeend', container);
+
+        const container2 = document.querySelectorAll("#main_deux_partie .image_arrow_card");
+        for (let i = 0; i < 4; i++) {
+            createCarousel(container2[i]);
+            console.log("carrou");
+            console.log(container2[i]);
+            console.log(container2.length);
+        }
     }
 }
 
-
-
-
-// Ajout de l'écouteur d'événements scroll
 window.addEventListener("scroll", handleScroll);
-
-// Retrait de l'écouteur d'événements scroll
 
